229a035678d233187ea7d32158405c52fc504c80
jokerguo <1977036313@qq.com>
2020-03-14 21:20:08 +0800

    deploy

---
+++ precache-manifest.17fa68178935b03c9a9803bb5a7f40ca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "719d2cc0e6fdded513cb",
+    "url": "/manager-website/css/app.52206d38.css"
+  },
+  {
+    "revision": "186888193e111a70eb545b1f82635173",
+    "url": "/manager-website/index.html"
+  },
+  {
+    "revision": "719d2cc0e6fdded513cb",
+    "url": "/manager-website/js/app.9f0afbce.js"
+  },
+  {
+    "revision": "8cc67e4a651e0163f23f",
+    "url": "/manager-website/js/chunk-vendors.bb19d1e8.js"
+  },
+  {
+    "revision": "8742eceeec9ed7941cf829cfab258c2f",
+    "url": "/manager-website/manifest.json"
+  },
+  {
+    "revision": "cfac0079059316be5d1f5781a760d575",
+    "url": "/manager-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/manager-website/robots.txt"
+  }
+]);
